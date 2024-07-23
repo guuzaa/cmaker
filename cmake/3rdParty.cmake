6144e02d75aec3bf0061ea3c7b0bcc76acfbc206
guza <guza@duck.com>
2024-07-24 07:31:22 +0800

    refeact cmakelists files for 3rd_party

--- cmake/3rdParty.cmake
+++ cmake/3rdParty.cmake
@@ -3,20 +3,20 @@ set(benchmark_version 1.8.5)
 
 include(FetchContent)
 
-function(include_googletest)
-    FetchContent_Declare(
-        googletest
-        URL "https://github.com/google/googletest/archive/refs/tags/v${googletest_version}.zip"
-    )
+FetchContent_Declare(
+    googletest
+    URL "https://github.com/google/googletest/archive/refs/tags/v${googletest_version}.zip"
+)
 
+FetchContent_Declare(
+    benchmark
+    URL "https://github.com/google/benchmark/archive/refs/tags/v${benchmark_version}.zip"
+)
+
+function(include_googletest)
     FetchContent_MakeAvailable(googletest)
 endfunction(include_googletest)
 
 function(include_benchmark)
-    FetchContent_Declare(
-        benchmark
-        URL "https://github.com/google/benchmark/archive/refs/tags/v${benchmark_version}.zip"
-    )
-
     FetchContent_MakeAvailable(benchmark)
 endfunction(include_benchmark)
